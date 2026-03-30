801ee4bb4a5f62867d5529d1bfad5af971a5b9a4
Eric Ekholm <eric.ekholm@gmail.com>
2026-03-30 15:32:17 -0400

    even 2025-q2-p2: wrong but misread directions

--- event_2024/src/q2.jl
+++ event_2024/src/q2.jl
@@ -38,3 +38,54 @@ inp1 = readlines(inp_file_1)
 pi1 = PuzzleInput(inp1)
 
 a1 = count_matches(pi1)
+
+## Part 2 -----
+
+mutable struct PuzzleInput2
+    Keywords::Vector{String}
+    PuzzleLines::Vector{String}
+end
+
+function PuzzleInput2(x::Vector{String})
+    k = extract_keywords(x[1])
+    v = x[3:end]
+    return PuzzleInput2(k, v)
+end
+
+# count runes forward and backward
+# note that 'runes' is language from the problem -- we're *not* actually counting Julia runes
+function count_runes_f_b(needle::String, haystack::Vector{String})
+    rgx = Regex(needle)
+    n = length(haystack)
+    res = Vector{Integer}(undef, n)
+
+    for i in eachindex(haystack)
+        m_forward = length(collect(eachmatch(rgx, haystack[i], overlap=true)))
+        m_backward = length(collect(eachmatch(rgx, reverse(haystack[i]), overlap=true)))
+
+        res[i] = m_forward + m_backward
+    end
+
+    return sum(res)
+end
+
+function solve_p2(x::PuzzleInput2)
+    l = length(x.Keywords)
+    res = Vector{Integer}(undef, l)
+
+    for i in eachindex(x.Keywords)
+        res[i] = count_runes_f_b(x.Keywords[i], x.PuzzleLines)
+    end
+
+    return sum(res)
+end
+
+inp_file_2 = "event_2024/data/q2_2.txt"
+inp2 = readlines(inp_file_2)
+
+pi2 = PuzzleInput2(inp2)
+
+a2 = solve_p2(pi2)
+# ok, so this is currently wrong.
+# i'm counting the number of matches, but what I actually want is the number of unique characters that are part of a match
+# i'm not going to fix it now, but good to know for later :)
